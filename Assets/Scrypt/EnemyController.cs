31a5400fd82fddcd971e72751b85a236e9e01141
Hiromichi Yamamoto <th1khse@gmail.com>
2018-08-22 23:26:38 +0900

    2Dゲームsurviveの初めてのコミット
    プレイヤー、敵、アイテムを作成しコライダーを設定した

---
+++ Assets/Scrypt/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	GameObject Player;
+	GameObject Enemy;
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find ("Player");
+		Enemy = GameObject.Find ("Enemy");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector2.MoveTowards (this.transform.position,new Vector2(Player.transform.position.x, Player.transform.position.y), Speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scrypt/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float movespeed;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "EnemyTag"){
+			DestroyObject (gameObject);
+
+			Debug.Log ("hi");
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector2(moveHorizontal, moveVertical);
+
+		this.rb.AddForce (movement * movespeed);
+	}
+
+
+}
